fab39889f878f91a1015a6f73a77ebd9f761bec4
Abhishek Swain <abhiswain.it.2016@gmail.com>
2021-02-23 00:40:26 +0530

    added pultojl version of notes in julia

---
+++ preparatory_sesisons/Co-ordinate-Geometry.jl
@@ -0,0 +1,65 @@
+### A Pluto.jl notebook ###
+# v0.11.10
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 6ef30220-7474-11eb-3d80-e9149a026851
+md"""
+``\large y = mx + c``
+
+The equation given is the equation for a straight line. 
+- **m** -> slope
+- **c** -> y-intercept
+- **x** -> x-coordinate.
+
+#### **Points to be noted**
+1. Slope is same everywhere on a line
+2. Two parellel lines have the same slope
+3. Intersecting lines have different slopes
+
+### General form of the equation of a straight line
+
+`` \large ax + by + c = 0 ``
+
+Rearranging to get it into the slope - intercept form gives:
+
+`` \large y = \frac{-c-ax}{b} = \frac{-a}{b}x + \frac{-c}{b} ``
+
+Here, m = `` \Large \frac{-a}{b} `` & `` c = \Large \frac{-c}{b} ``
+
+If we generalize the equation to *n* dimensions
+
+`` \large w_1x_1 + w_2x_2 + \dots + w_0 = 0 ``
+
+`` \large x_1, x_2, \dots ``
+
+`` \large w_1, w_2, \dots ``
+
+# Equation of a plane 
+
+`` \large \pi : ax + by + cz + d = 0 ``
+
+`` \large L: ax + by + c = 0 ``
+
+`` \large \pi `` is used to represent plane 
+
+We can also write it as a more general notation:
+
+`` \large \pi : w_1x_1 + w_2x_2 + w_3x_3 + w_0 = 0 ``
+
+
+# Equation of a hyperplane
+
+When the number of dimensions is greater than 2, then we call it **hyper-plane**
+It is represented by: `` \large \pi_d ``
+
+It's equation is:
+
+`` \large \pi_d : w_1x_1 + w_2x_2 + \dots + w_dx_d + w_0= 0 ``
+
+### Vectors in Linear Algebra
+"""
+
+# ╔═╡ Cell order:
+# ╟─6ef30220-7474-11eb-3d80-e9149a026851
