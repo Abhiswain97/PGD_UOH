4df886516661ad27e8468686dc70bd1f47b567ec
Abhishek Swain <abhiswain.it.2016@gmail.com>
2021-02-25 14:46:27 +0530

    upload files

---
+++ insertion_sort.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+def insertionSort(arr: List):
+    for i in range(1, len(arr)):
+        key = arr[i]
+
+        j = i - 1
+
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+
+            j -= 1
+
+        arr[j + 1] = key
+
+
+if __name__ == "__main__":
+    a = [6, 5, 3, 1, 8, 7, 2, 4]
+    print(a)
+    insertionSort(a)
+    print(a)
