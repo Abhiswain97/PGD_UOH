a203225ffe6be36523d3fc649476dc3407123e5b
Abhishek Swain <abhiswain.it.2016@gmail.com>
2021-03-04 23:28:46 +0530

    clean code more

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,6 +1,11 @@
 from typing import List
 
 
+def printList(arr: List):
+    for i in arr:
+        print(i, end=" ")
+
+
 def insertionSort(arr: List):
     for i in range(1, len(arr)):
         key = arr[i]
@@ -18,5 +23,6 @@ def insertionSort(arr: List):
 if __name__ == "__main__":
     a = [6, 5, 3, 1, 8, 7, 2, 4]
     print(a)
+
     insertionSort(a)
-    print(a)
+    printList(a)
