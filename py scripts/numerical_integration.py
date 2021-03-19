fixed numerical_integration

py scripts/numerical_integration.py
@@ -1,17 +1,25 @@
 # %%
-import math
+import numpy as np
+import matplotlib.pyplot as plt
 
 # %%
 def f(x):
-    if x != 0:
-        return math.sin(x) / x
-    else:
-        return math.nan
+    return np.sin(x) / x
 
 
 # Integrate f(x) from -10 to 10
 
 f(10)
+
+#%%
+
+x = np.arange(-10, 10)
+
+plt.plot(x, f(x))
+plt.grid()
+plt.show()
+
+
 # %%
 x = -10
 area = 0