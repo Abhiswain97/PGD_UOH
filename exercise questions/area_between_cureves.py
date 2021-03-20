cb4b8825283eb876c14177d557d9de3ce0172e36
Abhishek Swain <abhiswain.it.2016@gmail.com>
2021-03-20 17:15:55 +0530

    added area between curves

---
+++ exercise questions/area_between_cureves.py
@@ -0,0 +1,45 @@
+#%%
+
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+#%%
+
+
+def f1(x):
+    return np.exp(x)
+
+
+def f2(x):
+    return x ** 3
+
+
+def newFunc(x):
+    return abs(f1(x) - f2(x))
+
+
+#%%
+
+x = np.linspace(0, 4)
+
+plt.plot(x, f1(x))
+plt.plot(x, f2(x))
+plt.grid(True)
+plt.show()
+
+
+# %%
+
+x = 0
+area = 0
+delta = 0.0001
+
+while x <= 4:
+
+    area += (newFunc(x) + newFunc(x + delta)) * (1 / 2) * delta
+
+    x += delta
+
+print(f"Aread under the curve is: {area}")
+# %%
