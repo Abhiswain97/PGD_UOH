0418c42d03641eb952d52cdf5ce9752df0e70e9e
Abhishek Swain <abhiswain.it.2016@gmail.com>
2021-03-23 01:27:17 +0530

    regex tut

---
+++ regex_tutorial.py
@@ -0,0 +1,47 @@
+# %%
+
+import re
+# %%
+
+text_to_search = '''
+abcdefghijklmnopqurtuvwxyz
+ABCDEFGHIJKLMNOPQRSTUVWXYZ
+1234567890
+Ha HaHa
+MetaCharacters (Need to be escaped):
+. ^ $ * + ? { } [ ] \ | ( )
+coreyms.com
+321-555-4321
+123.555.1234
+123*555*1234
+800-555-1234
+900-555-1234
+Mr. Schafer
+Mr Smith
+Ms Davis
+Mrs. Robinson
+Mr. T
+'''
+
+# %%
+
+pattern = re.compile(r'abc')
+
+matches = pattern.finditer(text_to_search)
+
+# %%
+
+for match in matches:
+    print(match)
+# %%
+
+
+print(text_to_search[1:4])
+# %%
+
+pattern1 = re.compile('\.')
+
+pattern1.findall(text_to_search)
+# %%
+
+re.compile('.')
